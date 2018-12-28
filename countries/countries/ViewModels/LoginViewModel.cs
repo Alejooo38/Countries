9b1e4f6d51fa069906c4b34ed45b1898af6df33f
Alejooo38 <37305614+Alejooo38@users.noreply.github.com>
2018-12-28 09:34:56 -0500

    Ending Login

---
+++ countries/countries/ViewModels/LoginViewModel.cs
@@ -0,0 +1,115 @@
+﻿namespace countries.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Xamarin.Forms;    
+
+    public class LoginViewModel : BaseViewModel
+    {
+        //Aqui se deben declarar las propiedades que se bindaron en
+        //la LoginPage "Deben ser del mismo tipo string, booleano etc."
+        
+        #region Attributes
+            private string email;
+            private string password;
+            private bool isRunning;
+            private bool isEnable;
+        #endregion
+
+        #region Properties
+            public string Email
+            {
+                get { return email; }
+                set { SetValue(ref email, value); }
+            }
+
+        public string Password
+            {
+                get { return password; }
+                set { SetValue(ref password, value); }
+            }
+
+        public bool IsRunning
+            {
+                get { return this.isRunning; }
+                set { SetValue(ref isRunning, value); }
+            }
+
+        public bool IsRemmembered
+            {
+                get;
+                set;
+            }
+
+        public bool IsEnable
+            {
+                get { return this.isEnable; }
+                set { SetValue(ref isEnable, value); }
+            }
+        #endregion
+
+        #region Constructors
+            public LoginViewModel()
+            {
+                this.IsRemmembered = true;
+                this.IsEnable = true;
+            }
+        #endregion
+
+        #region Commands
+            public ICommand LoginCommand
+            {
+                get
+                {
+                    return new RelayCommand(Login);
+                }
+            }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email....",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password....",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnable = false;
+
+            if (this.Email != "druizbermud@uniminuto.edu.co" ||
+                this.Password != "123")
+            {
+                this.IsRunning = false;
+                this.IsEnable = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or Password wrong....",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnable = true;
+            await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You are in!!you're on fire!!",
+                    "Accept");
+        }
+        #endregion
+
+    }
+}
--- countries/countries/ViewModels/MainViewModel.cs
+++ countries/countries/ViewModels/MainViewModel.cs
@@ -1,10 +1,22 @@
 ﻿namespace countries.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+
     }
 }
